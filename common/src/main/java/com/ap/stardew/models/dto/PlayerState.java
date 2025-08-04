7a4b27ebac7334e9602f3d26e94ebe9656261e73
AliAlmasiZ <Ali1385Almasi@gmail.com>
2025-08-04 20:03:43 +0330

    move some client models to common ( I hate this commit )

--- common/src/main/java/com/ap/stardew/models/dto/PlayerState.java
+++ common/src/main/java/com/ap/stardew/models/dto/PlayerState.java
@@ -1,13 +1,19 @@
 package com.ap.stardew.models.dto;
 
+import com.badlogic.gdx.math.Vector2;
+
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.Map;
 
 public class PlayerState implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
 
     public String username;
-    public float x, y;
+    public Vector2 position;
+    public float energy;
+
+    public Map<Integer, Integer> inventory
 
 }
